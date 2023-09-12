1ed6d2ded8a32d1bd4fb39e0e9f413a8627ab9ee
SamuliK <samuli@karevaara.net>
2023-09-12 16:31:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,6 @@ import streamlit as st
 import requests
 import json
 
-#st.set_page_config(
-#    page_title="Ex-stream-ly Cool App",
-#    page_icon="🧊",
-#    layout="wide",
-#    initial_sidebar_state="expanded",
-#    menu_items={
-#        'Get Help': 'https://www.extremelycoolapp.com/help',
-#        'Report a bug': "https://www.extremelycoolapp.com/bug",
-#        'About': "# This is a header. This is an *extremely* cool app!"
-#    }
-#)
-
 st.set_page_config(
     page_title="Tilastokeskuksen taulukkohaku",
 )
